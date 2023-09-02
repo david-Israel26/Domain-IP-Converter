81a66bd7a65a4d52cce2530b5cb2c170cad3ae20
david-Israel26 <118939519+david-Israel26@users.noreply.github.com>
2023-09-02 16:56:17 -0600

    1 file modified

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ async function fetchIPAddress(domain) {
     `https://cloudflare-dns.com/dns-query?name=${domain}&type=A`,
     {
       headers: {
+        //Contenido esperado -> JSON
         accept: 'application/dns-json',
       },
     }
@@ -16,13 +17,20 @@ async function fetchIPAddress(domain) {
 }
 
 //Obteniendo la referencia a la caja de texto
+//Obten el elemento html con id domain y asignalo a la variable domain
 let domain = document.getElementById('domain');
+//Obten el elemento html con id ip y asignalo a la variable ipSpan
 let ipSpan = document.getElementById('ip');
+//Obten el elemento html con id convert y asignalo a la variable convertBtn
 let convertBtn = document.getElementById('convert');
 
+//Evento click utilizando callbacks con una arrow function
 convertBtn.addEventListener('click', async (event) => {
+  //Espera a obtener el valor del dominio
   const ipAddress = await fetchIPAddress(domain.value);
+  //Envialo a la consola
   console.log(`Data: ${ipAddress}`);
+  //En caso de algun error mostrar el mensaje
   ipSpan.innerText = !ipAddress
     ? 'something went wrong in fetchIPAddress'
     : ipAddress;
